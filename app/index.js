519f521417d04d94761469614a96cd9d51901564
Kyle Henderson <kyle@kyles-mbp.sces.uc.edu>
2017-01-25 10:41:49 -0500

    test

--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
 const discord = require('./lib/discord')
     , commands = require('./commands');
 
-require('dotenv').config();
-
 discord.on('message', commands);
 
 const github = require('./watchers/github');
